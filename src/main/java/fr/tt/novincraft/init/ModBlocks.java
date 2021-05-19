f98b780de9eabaf89a32d04e70d6e236e8e6b33b
Elios50 <eliosfilip@yahoo.fr>
2021-05-20 00:55:11 +0200

    Minerai cuissable + minable + craft block  to ingot

--- src/main/java/fr/tt/novincraft/init/ModBlocks.java
+++ src/main/java/fr/tt/novincraft/init/ModBlocks.java
@@ -18,9 +18,9 @@ public class ModBlocks {
 
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, NV.MODID);
 
-    public static final RegistryObject<Block> COPPER_BLOCK = createBlock("copper_block", () -> new Block(AbstractBlock.Properties.create(Material.IRON).hardnessAndResistance(3f, 15f).harvestTool(ToolType.PICKAXE).harvestLevel(2))); // Création d'un nouveau block : nom="copper_block" résistance aux éxplosions ="3" Résistance ="15" Type d'outils ="pioche" Niveau de minage ="3 - fer"
+    public static final RegistryObject<Block> COPPER_BLOCK = createBlock("copper_block", () -> new Block(AbstractBlock.Properties.create(Material.IRON).hardnessAndResistance(3f, 15f).harvestTool(ToolType.PICKAXE).setRequiresTool().harvestLevel(2))); // Création d'un nouveau block : nom="copper_block" résistance aux éxplosions ="3" Résistance ="15" Type d'outils ="pioche" Niveau de minage ="3 - fer"
 
-    public static final RegistryObject<Block> COPPER_ORE = createBlock("copper_ore", () -> new Block(AbstractBlock.Properties.create(Material.IRON).hardnessAndResistance(3f, 15f).harvestTool(ToolType.PICKAXE).setRequiresTool())); // Création d'un nouveau block : nom="copper_ore" résistance aux éxplosition ="3" Résistance ="15" Type d'outils ="pioche" Niveau de minage ="3 - fer"
+    public static final RegistryObject<Block> COPPER_ORE = createBlock("copper_ore", () -> new Block(AbstractBlock.Properties.create(Material.IRON).hardnessAndResistance(3f, 15f).harvestTool(ToolType.PICKAXE).setRequiresTool().harvestLevel(2))); // Création d'un nouveau block : nom="copper_ore" résistance aux éxplosition ="3" Résistance ="15" Type d'outils ="pioche" Niveau de minage ="3 - fer"
 
     public static RegistryObject<Block> createBlock(String name, Supplier<? extends Block> supplier)
     {
